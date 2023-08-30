cc3ffdf263c25e01e2c33a2e78cc5c5caa6b35f7
Martin Kainzbauer <m.kainzbauer@xtools.at>
2023-08-30 16:11:26 +0200

    fix: improvements to regular ONFT contracts

--- contracts/token/onft/extension/ExtendedONFT721.sol
+++ contracts/token/onft/extension/ExtendedONFT721.sol
@@ -12,48 +12,17 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract ExtendedONFT721 is Ownable, AccessControl, ERC721, ERC721Royalty, ERC721Enumerable, ERC721Burnable, ONFT721 {
     using Strings for uint256;
 
-    string internal baseUri;
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    string internal baseTokenURI;
 
-    constructor(string memory _name, string memory _symbol, string memory _baseUri, uint256 _minGasToTransfer, address _lzEndpoint) ONFT721(_name, _symbol, _minGasToTransfer, _lzEndpoint) {
+    constructor(string memory _name, string memory _symbol, string memory _baseUri, uint96 _royaltyBasePoints, uint256 _minGasToTransfer, address _lzEndpoint) ONFT721(_name, _symbol, _minGasToTransfer, _lzEndpoint) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _setupRole(MINTER_ROLE, _msgSender());
-        _setDefaultRoyalty(_msgSender(), 200); // 2%
-        baseUri = _baseUri;
+        _setDefaultRoyalty(_msgSender(), _royaltyBasePoints);
+        baseTokenURI = _baseUri;
     }
 
     /********************************************
      *** Public functions
      ********************************************/
-
-    /**
-     * @dev Public minting method, Minter-role only.
-     */
-    function mint(address to, uint256 tokenId) public virtual onlyRole(MINTER_ROLE) {
-        _safeMint(to, tokenId, "");
-    }
-
-    /**
-     * @dev Multi-recipient minting.
-     */
-    function mintMulti(address[] memory recipients, uint256[] memory tokenIds) public virtual onlyRole(MINTER_ROLE) {
-        require(recipients.length > 0 && recipients.length == tokenIds.length, "ERC721: input length mismatch");
-
-        for (uint16 i = 0; i < tokenIds.length; i++) {
-            _safeMint(recipients[i], tokenIds[i], "");
-        }
-    }
-
-    /**
-     * @dev Batch-Mint (same recipient).
-     */
-    function mintBatch(address to, uint256[] memory tokenIds) public virtual onlyRole(MINTER_ROLE) {
-        require(tokenIds.length > 0, "ERC721: tokenIds can't be empty");
-
-        for (uint16 i = 0; i < tokenIds.length; i++) {
-            _safeMint(to, tokenIds[i], "");
-        }
-    }
 
     /**
      * @dev Multi-recipient transfer.
@@ -78,19 +47,20 @@ contract ExtendedONFT721 is Ownable, AccessControl, ERC721, ERC721Royalty, ERC72
     }
 
     /**
-     * @dev Set new metadata host.
+     * @dev Set new token metadata base URI.
      */
     function setBaseURI(string memory _baseUri) public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
-        baseUri = _baseUri;
+        baseTokenURI = _baseUri;
     }
 
     /**
-     * @dev See {IERC721Metadata-tokenURI}. Override uses `baseUri` variable and attaches ".json" extension to URI.
+     * @dev See {IERC721Metadata-tokenURI}. Override attaches ".json" extension to URI.
      */
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         _requireMinted(tokenId);
 
-        return bytes(baseUri).length > 0 ? string(abi.encodePacked(baseUri, tokenId.toString(), ".json")) : "";
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
     }
 
     /**
@@ -103,6 +73,13 @@ contract ExtendedONFT721 is Ownable, AccessControl, ERC721, ERC721Royalty, ERC72
     /********************************************
      *** Internal functions
      ********************************************/
+
+    /**
+     * @dev Updateable base token URI override.
+     */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
 
     /**
      * @dev See {ERC721-_burn}.
